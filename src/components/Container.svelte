a5e0b3d8ccd90c4e8153e407655b4ad6d9efd4c1
harshadaju@gmail.com <harshadaju@gmail.com>
2020-09-09 15:38:18 +0530

    initial commit

---
+++ src/components/Container.svelte
@@ -0,0 +1,16 @@
+<script>
+  let w = 1000,
+    h;
+</script>
+
+<style>
+  div {
+    background-color: red;
+  }
+</style>
+
+<div style="width:{w}px;height:{h}px">
+  hello from container {w} x {h}
+  <input type="number" bind:value={w} />
+  <input type="number" bind:value={h} />
+</div>
